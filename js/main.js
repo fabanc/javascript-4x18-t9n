b8e17bcfc8f05f7758d1dd6ecf6e1a094dc9e51d
fabanc <fabien.ancelin@gmail.com>
2021-03-08 08:49:39 -0500

    Initial Commit - No Custom Widget

---
+++ js/main.js
@@ -0,0 +1,16 @@
+define(["require", "exports", "tslib", "esri/Map", "esri/views/MapView"], function (require, exports, tslib_1, Map_1, MapView_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    Map_1 = tslib_1.__importDefault(Map_1);
+    MapView_1 = tslib_1.__importDefault(MapView_1);
+    var map = new Map_1.default({
+        basemap: "streets"
+    });
+    var view = new MapView_1.default({
+        map: map,
+        container: "viewDiv",
+        center: [-118.244, 34.052],
+        zoom: 12
+    });
+});
+//# sourceMappingURL=main.js.map
