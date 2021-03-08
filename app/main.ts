faf1b816c71032d79f73489b5ae95b1756a04640
fabanc <fabien.ancelin@gmail.com>
2021-03-08 09:00:25 -0500

    Added recenter with no internationalization

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 import EsriMap from "esri/Map";
 import MapView from "esri/views/MapView";
+import Recenter from "./custom_widgets/Recenter"
 
 const map = new EsriMap({
   basemap: "streets"
@@ -11,3 +12,9 @@ const view = new MapView({
   center: [-118.244, 34.052],
   zoom: 12
 });
+
+const recenter = new Recenter(
+  {view: view, initialCenter: [-100.33, 43.69]
+}); 
+
+view.ui.add(recenter, 'top-right');
--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-define(["require", "exports", "tslib", "esri/Map", "esri/views/MapView"], function (require, exports, tslib_1, Map_1, MapView_1) {
+define(["require", "exports", "tslib", "esri/Map", "esri/views/MapView", "./custom_widgets/Recenter"], function (require, exports, tslib_1, Map_1, MapView_1, Recenter_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     Map_1 = tslib_1.__importDefault(Map_1);
     MapView_1 = tslib_1.__importDefault(MapView_1);
+    Recenter_1 = tslib_1.__importDefault(Recenter_1);
     var map = new Map_1.default({
         basemap: "streets"
     });
@@ -12,5 +13,8 @@ define(["require", "exports", "tslib", "esri/Map", "esri/views/MapView"], functi
         center: [-118.244, 34.052],
         zoom: 12
     });
+    var recenter = new Recenter_1.default({ view: view, initialCenter: [-100.33, 43.69]
+    });
+    view.ui.add(recenter, 'top-right');
 });
 //# sourceMappingURL=main.js.map
